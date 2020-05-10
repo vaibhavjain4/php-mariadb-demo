885bae9232ed02f338cca0bd560b231b48acf9ed
Vaibhav Jain <vaibhavjain4@users.noreply.github.com>
2020-05-10 17:58:12 +0530

    Update getStudentData.php

--- getStudentData.php
+++ getStudentData.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=mariadb.php-mariadb.svc.cluster.local; port=3306 ;dbname=studentdb; charset=utf8";
+$dsn = "mysql:host=studentdb.user1-project1.svc.cluster.local; port=3306 ;dbname=studentdb; charset=utf8";
 $username = "dbadmin";
 $password = "dbpassword";
 
